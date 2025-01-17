afd57d7a8e0f6619cb0744a52d31df688c844f84
anaim4897 <anaim4897@gmail.com>
2025-01-17 11:46:03 +0000

    finished task

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from room import Room
 
 kitchen = Room("kitchen")
 
-ballroom = Room("ballroom")
-dining_hall = Room("dining hall")
-
+kitchen = Room("kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies")
+ballroom = Room("ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor")
+dining_hall = Room("dining hall")
+dining_hall.set_description("A large room with ornate golden decorations")
 
-kitchen.describe()
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom,"west")
+ballroom.link_room(dining_hall,"east")
+
+dining_hall.get_details()
